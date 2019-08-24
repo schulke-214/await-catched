642dcbf1e2e515c800ac323a32f709341d4eec42
schulke-214 <schulkemaximilian@gmail.com>
2019-08-24 18:30:52 +0200

    feat(examples): add the advanced example

---
+++ examples/advanced.js
@@ -0,0 +1,23 @@
+const { catched } = require('../dist');
+
+const action = async num => {
+	await new Promise((resolve, reject) => {
+		if (num > 3) reject(num);
+
+		setTimeout(() => resolve, 2000);
+	});
+
+	console.log('ACTION HAPPENED', num);
+
+	return num;
+};
+
+(async () => {
+	const errorHandler = e => {
+		console.log('ACTION FAILED:', e);
+	};
+
+	await catched(action(54), errorHandler);
+	await catched(action(5), errorHandler);
+	await catched(action(2), errorHandler);
+})();
